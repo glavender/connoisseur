bd538168d6c36c471d80f17ce845082838589f6b
Graham Lavender <glavender@gmail.com>
2013-11-16 17:15:54 -0500

    Completed Connoisseur

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,13 +3,17 @@ require 'open-uri'
 class ProductsController < ApplicationController
   helper ProductsHelper
   def index
-    products_json = open('http://lcboapi.com/products?per_page=100').read
+    products_json = open('http://lcboapi.com/products?page=' + "#{params[:page]}").read
     @products = JSON.parse(products_json)
   end
 
   def show
     product_json = open('http://lcboapi.com/products/' + "#{params[:id]}").read
     @product = JSON.parse(product_json)
+    locations_json = open('http://lcboapi.com/products/' + "#{params[:id]}" + '/stores').read
+    @locations = JSON.parse(locations_json)
+    inventories_json = open('http://lcboapi.com/products/' + "#{params[:id]}" + '/inventories').read
+    @inventories = JSON.parse(inventories_json)
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Connoisseur::Application.routes.draw do
   root :to => "products#index"
   resources :products
+  resources :stores
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
